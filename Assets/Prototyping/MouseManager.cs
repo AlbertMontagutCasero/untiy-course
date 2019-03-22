36030eb556edce530ff451f008d98b4a1d5d32e2
Raikish <albert.montagut.casero@gmail.com>
2019-03-22 18:18:16 +0100

    Added Mouse Manager and implemented Swap Cursor

---
+++ Assets/Prototyping/MouseManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    //What Objects are clickable
+    public LayerMask clickableLayer;
+
+    //Swap cursors per objects # Icons
+    public Texture2D pointer; // Normal Pointer
+    public Texture2D target; // Cursor for clickable objects like the world
+    public Texture2D doorway; // Cursor for foorways
+    public Texture2D combat; // Cursor for combat actions
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
