88d2ef96eecdeeae4c25531c8da38cbc99cd815a
Raikish <albert.montagut.casero@gmail.com>
2019-03-22 18:16:34 +0100

    Forward vector is Z(blue) test

---
+++ Assets/Prototyping/TestMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = transform.position + Vector3.forward * 1 * Time.deltaTime;
+    }
+}
